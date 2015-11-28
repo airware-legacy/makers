b147bf6aa2bdbd12fc2f2144536893bbb61ff4b5
Eric Johnson <edj.boston@gmail.com>
2015-11-27 18:54:03 -0800

    Gulp lint and test tasks (and missing gulp dep)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
-var del  = require('del');
+var del  = require('del'),
+	eslint = require('gulp-eslint'),
+	gulp = require('gulp'),
+    mocha = require('gulp-mocha');
 
 
 // Clean the build dir
 gulp.task('clean', function() {
 	return del('build/**');
+});
+
+
+// Run tests and product coverage
+gulp.task('test', function () {
+    return gulp.src(['test/*.js'])
+        .pipe(mocha());
+});
+
+
+// Lint as JS files (including this one)
+gulp.task('lint', function () {
+    return gulp.src([
+            '**/*.js',
+            '!node_modules/**'
+        ])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
