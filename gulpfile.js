452e9edf0de0060b2e339a550e58539c969129a0
Eric Johnson <edj.boston@gmail.com>
2015-11-27 19:35:18 -0800

    Gulp LESS dep, concat/minify Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-var del  = require('del'),
+var concat = require('gulp-concat'),
+	del  = require('del'),
 	eslint = require('gulp-eslint'),
 	gulp = require('gulp'),
+	gulpif = require('gulp-if'),
+	less = require('gulp-less'),
+	minifyCSS = require('gulp-minify-css'),
     mocha = require('gulp-mocha');
 
 
@@ -26,4 +30,18 @@ gulp.task('lint', function () {
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
+});
+
+// Minify and combine all CSS
+gulp.task('styles', ['clean'], function() {
+    return gulp.src([
+            'node_modules/bootstrap/dist/css/bootstrap.css',
+            'src/less/custom.less'
+        ])
+    	.pipe(gulpif(function(file) {
+    			return /(.*).less/.test(file.path);
+    		}, less()))
+        .pipe(minifyCSS())
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest('build/css'));
 });
