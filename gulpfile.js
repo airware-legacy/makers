070a7b72295f03a003b209c765c95cc4aed613d9
Eric Johnson <edj.boston@gmail.com>
2015-12-10 00:21:54 -0800

    Dynamic robots.txt content for staging

--- gulpfile.js
+++ gulpfile.js
@@ -85,6 +85,11 @@ gulp.task('scripts', function() {
 // Copy static files to build dir
 gulp.task('static', function() {
     return gulp.src('src/static/**')
+        .pipe(gulpif(/robots\.txt/, tap(function(file) {
+            if ( process.env.TRAVIS_BRANCH == 'develop' ) {
+                file.contents = new Buffer('User-agent: *\nDisallow: /');
+            }
+        })))
         .pipe(gzip({ append: false }))
         .pipe(gulp.dest('build'));
 });
