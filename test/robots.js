5dc343436e2b2018a20469b54a2e1ce5c8436605
Eric Johnson <edj.boston@gmail.com>
2015-12-23 23:12:35 -0800

    Robots.txt tests

---
+++ test/robots.js
@@ -0,0 +1,35 @@
+var fs = require('fs'),
+    should = require('should'),
+    zlib = require('zlib');
+
+
+describe('The robots.txt file...', function() {
+
+    var handle = 'build/robots.txt';
+    var buf = null;
+    var str = '';
+
+    it('Should exist', function(done) {
+        fs.readFile(handle, function(err, data) {
+            if (err) throw err;
+            buf = data;
+            done();
+        });
+    });
+
+    it('Should be gzipped', function(done) {
+        zlib.gunzip(buf, function(err, data) {
+            if (err) throw err;
+            str = data.toString();
+            done();
+        });
+    });
+
+    it('Should contain the correct dynamic content', function() {
+        if ( process.env.TRAVIS_BRANCH == 'master' )
+            str.should.equal('');
+        else
+            str.should.equal('User-agent: *\nDisallow: /');
+    });
+
+});
