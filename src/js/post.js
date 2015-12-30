09f7a9d1a313b856a4e447ad9d8214c95a82cd2d
Nick Italiano <nitaliano@airware.com>
2015-12-30 13:10:01 -0800

    Updated table, list styles for a post

--- src/js/post.js
+++ src/js/post.js
@@ -9,22 +9,24 @@
         this.$el = $el;
         this.$imgs = this.$el.find('img');
         this.$links = this.$el.find('a');
+        this.$tables = this.$el.find('table');
 
+        this.initTables();
         this.initLinks();
     }
+
+    Post.prototype.initTables = function () {
+        this.$tables.each(function () {
+           $(this)
+               .addClass('table')
+               .wrap('<div class="table-responsive"></div>');
+        });
+    };
 
     Post.prototype.initLinks = function () {
         this.$links.each(function () {
             $(this).attr('target', '_blank');
         });
-    };
-
-    Post.prototype._getLargeImageURL = function (source) {
-        var sourceParts = source.split('.');
-        var ext = sourceParts.pop();
-        var filename = sourceParts.join('');
-
-        return filename + '_large.' + ext;
     };
 
     $(document).ready(function () {
