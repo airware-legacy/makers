a5375aa5d652bed2117d9302cbbbbf8e844e5963
Nick Italiano <nitaliano@airware.com>
2015-12-30 13:32:06 -0800

    Added animation for hero on page load

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,29 +5,47 @@
 (function () {
     'use-strict';
 
-    var headerCss = { opacity: 0 };
-
     function Layout() {
         this.$window = $(window);
         this.$header = $('.airware-header');
+        this.$hero = $('.airware-hero');
+
+        this.options = {
+            header: {
+                css: {
+                    opacity: 0
+                }
+            },
+            hero: {
+                transition: 'transition.slideDownBigIn',
+                duration: 500
+            }
+        };
+
         this.$window.scroll(this.onScroll.bind(this));
+        this.slideInHero();
     }
+
+    Layout.prototype.slideInHero = function () {
+       this.$hero.velocity(this.options.hero.transition, {
+           duration: this.options.hero.duration
+       });
+    };
 
     Layout.prototype.onScroll = function () {
         if (!this._isHeaderInViewport()) {
-            headerCss.opacity = 0;
+            this.options.header.css.opacity = 0;
         } else {
-            headerCss.opacity = 1;
+            this.options.header.css.opacity = 1;
         }
-        this.$header.css(headerCss);
+        this.$header.css(this.options.header.css);
     };
 
     Layout.prototype._isHeaderInViewport = function () {
         return this.$header.outerHeight() >= this.$window.scrollTop();
     };
 
-    var $document = $(document);
-    $document.ready(function () {
+    $(document).ready(function () {
         new Layout();
     });
 })();
