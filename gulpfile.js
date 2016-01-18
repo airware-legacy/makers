7bb8761cba1b2b73475f0605d9049d0b2525d66d
Eric Johnson <edj.boston@gmail.com>
2016-01-17 17:18:23 -0800

    Fixing AWS bucket region

--- gulpfile.js
+++ gulpfile.js
@@ -276,7 +276,7 @@ gulp.task('deploy', () => {
     var publisher = g.awspublish.create({
         accessKeyId     : process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
-        region          : 'us-west-2',
+        region          : 'us-east-1',
         params : {
             Bucket : argv.b
         }
